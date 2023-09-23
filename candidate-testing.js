e6d5200119070ef5a2e1075c475bf218d41bfa53
Jessica Orrick <jessica.orrick@outlook.com>
2023-09-23 15:17:01 -0500

    completed part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,13 +7,19 @@ let candidateName = ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -22,19 +28,22 @@ function askForName() {
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-
+//let candidateAnswer = []
+  for (let i = 0; i < questions.length; i++){
+    candidateAnswers.push(input.question(questions[i]));
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (correctAnswer.toLowerCase() === candidateAnswer.toLowerCase()){
-    console.log( `${candidateAnswer} is correct!`);
-  } else{
-    console.log(`${candidateAnswer} is incorrect.`)
-  }
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (correctAnswers[i].toString().toLowerCase() === candidateAnswers[i].toString().toLowerCase()){
+        console.log( `${candidateAnswers[i]} is correct!`);
+        } else{
+        console.log(`${candidateAnswers[i]} is incorrect.`)
+        }
+    }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -46,7 +55,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello ${candidateName}!`);
+  console.log(`Hello ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
